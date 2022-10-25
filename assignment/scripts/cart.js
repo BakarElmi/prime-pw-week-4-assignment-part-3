30ece73e376c2cd5df6010392e25085252eb0531
Bakar Elmi <bakarelmi1@gmil.com>
2022-10-25 16:38:41 -0500

    updated js file

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,35 @@
 console.log('***** Cart Functions *****');
+// Bakar Elmi
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+var basket = [];                   //global variable
+
+function addItem(item){            //function to add iteam
+    basket.push(item);
+    return true;
+}
+
+function listItems(basket){        //function to add iteam
+    for (let i=0; i < basket.length; i++){
+        console.log(basket[i]);
+    }
+}
+
+function empty(basket){            //function to empty basket
+    basket.length = 0;
+    return console.log('basket is now empty');
+}
+
+// testing code
+console.log(`Basket is: ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now: ${basket}`);
+console.log('Adding milk (expect true)', addItem('milk'));
+console.log('Adding fish (expect true)', addItem('fish'));
+console.log(`Basket is now: ${basket}`);
+empty(basket);                      // basket reset
+console.log(`Basket is: ${basket}`);
+console.log('Adding books (expect true)', addItem('books'));
+console.log(`Basket is: ${basket}`);
